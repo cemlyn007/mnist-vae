c09aaa6c5d769cafb2d8851f68e00ec5a0868979
Cemlyn <cemlynwaters@gmail.com>
2023-10-08 17:46:34 +0100

    refactor: move experiment code into `experiment.py`

---
+++ experiment.py
@@ -0,0 +1,255 @@
+from flax import linen as nn
+import jax
+import typing
+import mnist
+import jax.numpy as jnp
+import jax
+import optax
+import orbax.checkpoint as ocp
+import functools
+import operator
+
+
+class Encoder(nn.Module):
+    latent_dims: int
+
+    @nn.compact
+    def __call__(self, x: jax.Array) -> tuple[jax.Array, jax.Array]:
+        x = nn.Dense(980)(x)
+        x = nn.relu(x)
+        x = nn.Dense(1024)(x)
+        x = nn.relu(x)
+        x = nn.Dense(1280)(x)
+        x = nn.relu(x)
+        latent_mean_and_log_variance = nn.Dense(self.latent_dims + self.latent_dims)(x)
+        latent_mean, latent_log_variance = jnp.split(
+            latent_mean_and_log_variance, 2, axis=1
+        )
+        return latent_mean, latent_log_variance
+
+
+class Decoder(nn.Module):
+    @nn.compact
+    def __call__(self, x: jax.Array) -> jax.Array:
+        x = nn.Dense(1280)(x)
+        x = nn.relu(x)
+        x = nn.Dense(1024)(x)
+        x = nn.relu(x)
+        x = nn.Dense(784)(x)
+        return x
+
+
+class Hyperparameters(typing.NamedTuple):
+    latent_dims: int
+    learning_rate: float
+
+
+class ModelVariables(typing.NamedTuple):
+    encoder: dict[str, any]
+    decoder: dict[str, any]
+
+
+class State(typing.NamedTuple):
+    variables: ModelVariables
+    optimizer_state: optax.OptState
+    key: jax.random.PRNGKey
+    step: jax.Array
+
+
+class Experiment:
+    def __init__(
+        self,
+        hyperparameters: Hyperparameters,
+        checkpoint_directory: str,
+        cache_directory: str,
+        backend: str | None = None,
+    ) -> None:
+        self._device = jax.devices(backend)[0]
+        self._checkpoint_manager = self._get_checkpoint_manager(checkpoint_directory)
+        self._train_images = self._get_train_images(cache_directory)
+        self._encoder = self._get_encoder(hyperparameters.latent_dims)
+        self._decoder = self._get_decoder()
+        self._optimizer = self._get_optimizer(hyperparameters.learning_rate)
+
+        self._train_step = jax.jit(self._train_step, device=self._device)
+        self._predict = jax.jit(self._predict, device=self._device)
+
+    def reset(self, key: int) -> None:
+        if self._checkpoint_manager.directory.exists():
+            self._checkpoint_manager.directory.rmtree()
+        self._checkpoint_manager.directory.mkdir()
+        # else...
+        self._state = self._initial_state(jax.random.PRNGKey(key))
+
+    def restore(self, step: int | None = None) -> None:
+        state = self._initial_state(jax.random.PRNGKey(0))
+        if step is None:
+            step = self._checkpoint_manager.latest_step()
+
+        if step is not None:
+            self._state = self._checkpoint_manager.restore(
+                step,
+                items=state,
+            )
+        else:
+            raise RuntimeError("No checkpoint found!")
+
+    def close(self) -> None:
+        try:
+            step = self._state.step.item()
+            if self._checkpoint_manager.latest_step() != step:
+                self._checkpoint_manager.save(
+                    self._state.step.item(), self._state, force=True
+                )
+        finally:
+            self._checkpoint_manager.close()
+
+    def checkpoint_exists(self) -> bool:
+        return self._checkpoint_manager.latest_step() is not None
+
+    def _get_checkpoint_manager(
+        self, checkpoint_directory: str
+    ) -> ocp.CheckpointManager:
+        options = ocp.CheckpointManagerOptions(max_to_keep=3, save_interval_steps=100)
+        checkpoint_manager = ocp.CheckpointManager(
+            checkpoint_directory, ocp.PyTreeCheckpointer(), options=options
+        )
+        return checkpoint_manager
+
+    def _get_train_images(self, cache_directory: str) -> jax.Array:
+        dataset = mnist.Dataset(cache_directory)
+        dataset.download()
+        train_images = dataset.load_train_images(self._device)
+        train_images = train_images / 255.0
+        train_images = train_images.reshape(
+            (
+                train_images.shape[0],
+                functools.reduce(operator.mul, train_images.shape[1:], 1),
+            )
+        )
+        return train_images
+
+    def _get_encoder(self, latent_dims: int) -> Encoder:
+        return Encoder(latent_dims=latent_dims)
+
+    def _get_decoder(self) -> Decoder:
+        return Decoder()
+
+    def _get_optimizer(self, learning_rate: float) -> optax.GradientTransformation:
+        # return optax.inject_hyperparams(optax.adamaxw)(learning_rate=learning_rate)
+        return optax.inject_hyperparams(optax.adam)(learning_rate=learning_rate)
+
+    def _initial_state(
+        self,
+        key: jax.random.PRNGKey,
+    ) -> State:
+        key, encoder_init_key, decoder_init_key = jax.random.split(key, 3)
+        encoder_variables = self._encoder.init(
+            encoder_init_key,
+            jnp.empty(
+                (
+                    1,
+                    28 * 28,
+                ),
+                dtype=jnp.float32,
+            ),
+        )
+        decoder_variables = self._decoder.init(
+            decoder_init_key,
+            jnp.empty(
+                (
+                    1,
+                    self._encoder.latent_dims,
+                ),
+                dtype=jnp.float32,
+            ),
+        )
+        variables = ModelVariables(encoder=encoder_variables, decoder=decoder_variables)
+        optimizer_state = self._optimizer.init(variables)
+        return State(
+            variables=variables,
+            optimizer_state=optimizer_state,
+            key=key,
+            step=jnp.array(0, dtype=jnp.uint32),
+        )
+
+    def train_step(self) -> dict[str, any]:
+        self._state, metrics = self._train_step(self._state)
+        step = self._state.step.item()
+        self._checkpoint_manager.save(step, self._state, metrics=metrics)
+        return {"step": step, **metrics}
+
+    def _train_step(self, state: State) -> tuple[State, dict[str, any]]:
+        new_key, key = jax.random.split(state.key)
+        beta = 0.025
+        (loss, extras), grad = jax.value_and_grad(self._get_loss, has_aux=True)(
+            state.variables, key, beta
+        )
+        updates, new_optimizer_state = self._optimizer.update(
+            grad, state.optimizer_state
+        )
+        new_variables = optax.apply_updates(state.variables, updates)
+        next_state = state._replace(
+            variables=new_variables,
+            optimizer_state=new_optimizer_state,
+            key=new_key,
+            step=state.step + 1,
+        )
+        return next_state, {
+            "loss": loss,
+            "beta": beta,
+            "learning_rate": new_optimizer_state.hyperparams["learning_rate"],
+            **extras,
+        }
+
+    def _get_loss(
+        self, model_variables: ModelVariables, key: jax.random.PRNGKey, beta: float
+    ) -> tuple[jax.Array, dict[str, any]]:
+        sample_key, key = jax.random.split(key)
+        train_images = self._train_images[
+            jax.random.randint(sample_key, (8192,), 0, self._train_images.shape[0] + 1)
+        ]
+        latent_mean, latent_log_variance = self._encoder.apply(
+            model_variables.encoder, train_images
+        )
+        sampled_latent = latent_mean + jnp.exp(
+            latent_log_variance * 0.5
+        ) * jax.random.normal(key, latent_log_variance.shape)
+        predictions = self._decoder.apply(model_variables.decoder, sampled_latent)
+
+        binary_cross_entropy = optax.sigmoid_binary_cross_entropy(
+            predictions, train_images
+        )
+        mean_binary_cross_entropy = jnp.mean(binary_cross_entropy)
+        kl_divergence = -0.5 * jnp.mean(
+            1.0
+            + latent_log_variance
+            - jnp.square(latent_mean)
+            - jnp.exp(latent_log_variance)
+        )
+        loss = mean_binary_cross_entropy + beta * kl_divergence
+        return loss, {
+            "mean_binary_cross_entropy": mean_binary_cross_entropy,
+            "kl_divergence": kl_divergence,
+        }
+
+    def predict(self, key: int, image_indices: list[int]) -> jax.Array:
+        key = jax.random.PRNGKey(key)
+        return self._predict(self._state.variables, key, image_indices)
+
+    def _predict(
+        self,
+        model_variables: ModelVariables,
+        key: jax.random.PRNGKey,
+        image_indices: list[int],
+    ) -> jax.Array:
+        image_indices = jnp.array(image_indices)
+        latent_mean, latent_log_variance = self._encoder.apply(
+            model_variables.encoder, self._train_images[image_indices]
+        )
+        sampled_latent = latent_mean + jnp.exp(
+            latent_log_variance * 0.5
+        ) * jax.random.normal(key, latent_log_variance.shape)
+        predictions = self._decoder.apply(model_variables.decoder, sampled_latent)
+        predictions = 255.0 * nn.sigmoid(predictions)
+        return predictions
--- main.py
+++ main.py
@@ -1,262 +1,5 @@
-from flax import linen as nn
-import jax
-import typing
-import os
-import mnist
-import jax.numpy as jnp
-import jax
-import optax
-import orbax.checkpoint as ocp
-import functools
-import operator
-
-
-class Encoder(nn.Module):
-    latent_dims: int
-
-    @nn.compact
-    def __call__(self, x: jax.Array) -> tuple[jax.Array, jax.Array]:
-        x = nn.Dense(980)(x)
-        x = nn.relu(x)
-        x = nn.Dense(1024)(x)
-        x = nn.relu(x)
-        x = nn.Dense(1280)(x)
-        x = nn.relu(x)
-        latent_mean_and_log_variance = nn.Dense(self.latent_dims + self.latent_dims)(x)
-        latent_mean, latent_log_variance = jnp.split(
-            latent_mean_and_log_variance, 2, axis=1
-        )
-        return latent_mean, latent_log_variance
-
-
-class Decoder(nn.Module):
-    @nn.compact
-    def __call__(self, x: jax.Array) -> jax.Array:
-        x = nn.Dense(1280)(x)
-        x = nn.relu(x)
-        x = nn.Dense(1024)(x)
-        x = nn.relu(x)
-        x = nn.Dense(784)(x)
-        return x
-
-
-class Hyperparameters(typing.NamedTuple):
-    latent_dims: int
-    learning_rate: float
-
-
-class ModelVariables(typing.NamedTuple):
-    encoder: dict[str, any]
-    decoder: dict[str, any]
-
-
-class State(typing.NamedTuple):
-    variables: ModelVariables
-    optimizer_state: optax.OptState
-    key: jax.random.PRNGKey
-    step: jax.Array
-
-
-class Experiment:
-    def __init__(
-        self,
-        hyperparameters: Hyperparameters,
-        checkpoint_directory: str,
-        cache_directory: str,
-        backend: str | None = None,
-    ) -> None:
-        self._device = jax.devices(backend)[0]
-        self._checkpoint_manager = self._get_checkpoint_manager(checkpoint_directory)
-        self._train_images = self._get_train_images(cache_directory)
-        self._encoder = self._get_encoder(hyperparameters.latent_dims)
-        self._decoder = self._get_decoder()
-        self._optimizer = self._get_optimizer(hyperparameters.learning_rate)
-
-        self._train_step = jax.jit(self._train_step, device=self._device)
-        self._predict = jax.jit(self._predict, device=self._device)
-
-    def reset(self, key: int) -> None:
-        if self._checkpoint_manager.directory.exists():
-            self._checkpoint_manager.directory.rmtree()
-        self._checkpoint_manager.directory.mkdir()
-        # else...
-        self._state = self._initial_state(jax.random.PRNGKey(key))
-
-    def restore(self, step: int | None = None) -> None:
-        state = self._initial_state(jax.random.PRNGKey(0))
-        if step is None:
-            step = self._checkpoint_manager.latest_step()
-
-        if step is not None:
-            self._state = self._checkpoint_manager.restore(
-                step,
-                items=state,
-            )
-        else:
-            raise RuntimeError("No checkpoint found!")
-
-    def close(self) -> None:
-        try:
-            step = self._state.step.item()
-            if self._checkpoint_manager.latest_step() != step:
-                self._checkpoint_manager.save(
-                    self._state.step.item(), self._state, force=True
-                )
-        finally:
-            self._checkpoint_manager.close()
-
-    def checkpoint_exists(self) -> bool:
-        return self._checkpoint_manager.latest_step() is not None
-
-    def _get_checkpoint_manager(
-        self, checkpoint_directory: str
-    ) -> ocp.CheckpointManager:
-        options = ocp.CheckpointManagerOptions(max_to_keep=3, save_interval_steps=100)
-        checkpoint_manager = ocp.CheckpointManager(
-            checkpoint_directory, ocp.PyTreeCheckpointer(), options=options
-        )
-        return checkpoint_manager
-
-    def _get_train_images(self, cache_directory: str) -> jax.Array:
-        dataset = mnist.Dataset(cache_directory)
-        dataset.download()
-        train_images = dataset.load_train_images(self._device)
-        train_images = train_images / 255.0
-        train_images = train_images.reshape(
-            (
-                train_images.shape[0],
-                functools.reduce(operator.mul, train_images.shape[1:], 1),
-            )
-        )
-        return train_images
-
-    def _get_encoder(self, latent_dims: int) -> Encoder:
-        return Encoder(latent_dims=latent_dims)
-
-    def _get_decoder(self) -> Decoder:
-        return Decoder()
-
-    def _get_optimizer(self, learning_rate: float) -> optax.GradientTransformation:
-        # return optax.inject_hyperparams(optax.adamaxw)(learning_rate=learning_rate)
-        return optax.inject_hyperparams(optax.adam)(learning_rate=learning_rate)
-
-    def _initial_state(
-        self,
-        key: jax.random.PRNGKey,
-    ) -> State:
-        key, encoder_init_key, decoder_init_key = jax.random.split(key, 3)
-        encoder_variables = self._encoder.init(
-            encoder_init_key,
-            jnp.empty(
-                (
-                    1,
-                    28 * 28,
-                ),
-                dtype=jnp.float32,
-            ),
-        )
-        decoder_variables = self._decoder.init(
-            decoder_init_key,
-            jnp.empty(
-                (
-                    1,
-                    self._encoder.latent_dims,
-                ),
-                dtype=jnp.float32,
-            ),
-        )
-        variables = ModelVariables(encoder=encoder_variables, decoder=decoder_variables)
-        optimizer_state = self._optimizer.init(variables)
-        return State(
-            variables=variables,
-            optimizer_state=optimizer_state,
-            key=key,
-            step=jnp.array(0, dtype=jnp.uint32),
-        )
-
-    def train_step(self) -> dict[str, any]:
-        self._state, metrics = self._train_step(self._state)
-        step = self._state.step.item()
-        self._checkpoint_manager.save(step, self._state, metrics=metrics)
-        return {"step": step, **metrics}
-
-    def _train_step(self, state: State) -> tuple[State, dict[str, any]]:
-        new_key, key = jax.random.split(state.key)
-        beta = 0.025
-        (loss, extras), grad = jax.value_and_grad(self._get_loss, has_aux=True)(
-            state.variables, key, beta
-        )
-        updates, new_optimizer_state = self._optimizer.update(
-            grad, state.optimizer_state
-        )
-        new_variables = optax.apply_updates(state.variables, updates)
-        next_state = state._replace(
-            variables=new_variables,
-            optimizer_state=new_optimizer_state,
-            key=new_key,
-            step=state.step + 1,
-        )
-        return next_state, {
-            "loss": loss,
-            "beta": beta,
-            "learning_rate": new_optimizer_state.hyperparams["learning_rate"],
-            **extras,
-        }
-
-    def _get_loss(
-        self, model_variables: ModelVariables, key: jax.random.PRNGKey, beta: float
-    ) -> tuple[jax.Array, dict[str, any]]:
-        sample_key, key = jax.random.split(key)
-        train_images = self._train_images[
-            jax.random.randint(sample_key, (8192,), 0, self._train_images.shape[0] + 1)
-        ]
-        latent_mean, latent_log_variance = self._encoder.apply(
-            model_variables.encoder, train_images
-        )
-        sampled_latent = latent_mean + jnp.exp(
-            latent_log_variance * 0.5
-        ) * jax.random.normal(key, latent_log_variance.shape)
-        predictions = self._decoder.apply(model_variables.decoder, sampled_latent)
-
-        binary_cross_entropy = optax.sigmoid_binary_cross_entropy(
-            predictions, train_images
-        )
-        mean_binary_cross_entropy = jnp.mean(binary_cross_entropy)
-        kl_divergence = -0.5 * jnp.mean(
-            1.0
-            + latent_log_variance
-            - jnp.square(latent_mean)
-            - jnp.exp(latent_log_variance)
-        )
-        loss = mean_binary_cross_entropy + beta * kl_divergence
-        return loss, {
-            "mean_binary_cross_entropy": mean_binary_cross_entropy,
-            "kl_divergence": kl_divergence,
-        }
-
-    def predict(self, key: int, image_indices: list[int]) -> jax.Array:
-        key = jax.random.PRNGKey(key)
-        return self._predict(self._state.variables, key, image_indices)
-
-    def _predict(
-        self,
-        model_variables: ModelVariables,
-        key: jax.random.PRNGKey,
-        image_indices: list[int],
-    ) -> jax.Array:
-        image_indices = jnp.array(image_indices)
-        latent_mean, latent_log_variance = self._encoder.apply(
-            model_variables.encoder, self._train_images[image_indices]
-        )
-        sampled_latent = latent_mean + jnp.exp(
-            latent_log_variance * 0.5
-        ) * jax.random.normal(key, latent_log_variance.shape)
-        predictions = self._decoder.apply(model_variables.decoder, sampled_latent)
-        predictions = 255.0 * nn.sigmoid(predictions)
-        return predictions
-
-
 if __name__ == "__main__":
+    import os
     import time
     import neptune
     import numpy as np
@@ -264,6 +7,7 @@ if __name__ == "__main__":
     import PIL.Image
     import shutil
     import platform
+    import experiment
 
     default_experiment_directory = os.path.join(os.getcwd(), "experiments")
     if platform.system() == "Darwin":
@@ -307,7 +51,7 @@ if __name__ == "__main__":
         if os.path.exists(last_neptune_run_path):
             os.remove(last_neptune_run_path)
         shutil.rmtree(experiment_directory, ignore_errors=True)
-        hyperparameters = Hyperparameters(latent_dims=16, learning_rate=1e-3)
+        hyperparameters = experiment.Hyperparameters(latent_dims=16, learning_rate=1e-3)
     else:
         if os.path.exists(last_neptune_run_path):
             with open(last_neptune_run_path, "r") as f:
@@ -323,7 +67,7 @@ if __name__ == "__main__":
                 mode="read-only",
             )
             try:
-                hyperparameters = Hyperparameters(
+                hyperparameters = experiment.Hyperparameters(
                     latent_dims=run["parameters/latent_dims"].fetch(),
                     learning_rate=run["parameters/learning_rate"].fetch(),
                 )
@@ -332,13 +76,15 @@ if __name__ == "__main__":
         else:
             raise RuntimeError("No last Neptune run found!")
 
-    experiment = Experiment(hyperparameters, checkpoint_path, cache_directory, backend)
+    this_experiment = experiment.Experiment(
+        hyperparameters, checkpoint_path, cache_directory, backend
+    )
     try:
-        if experiment.checkpoint_exists():
-            experiment.restore()
+        if this_experiment.checkpoint_exists():
+            this_experiment.restore()
         else:
             last_neptune_run = None
-            experiment.reset(0)
+            this_experiment.reset(0)
 
         run = neptune.init_run(
             with_id=last_neptune_run,
@@ -363,7 +109,7 @@ if __name__ == "__main__":
             while True:
                 profile.clear()
                 start_train_step = time.monotonic()
-                metrics = experiment.train_step()
+                metrics = this_experiment.train_step()
                 end_train_step = time.monotonic()
                 train_step_duration = end_train_step - start_train_step
                 profile["train_step_duration_ms"] = train_step_duration * 1000.0
@@ -377,7 +123,7 @@ if __name__ == "__main__":
                 if predict_interval and step % predict_interval == 0:
                     start_predict = time.monotonic()
                     image_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-                    predicted_images = experiment.predict(0, image_ids)
+                    predicted_images = this_experiment.predict(0, image_ids)
                     predicted_images = np.reshape(
                         np.round(np.asarray(predicted_images, dtype=np.float32)),
                         (-1, 28, 28),
@@ -407,4 +153,4 @@ if __name__ == "__main__":
         finally:
             run.stop()
     finally:
-        experiment.close()
+        this_experiment.close()
